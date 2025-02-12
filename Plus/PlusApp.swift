3a3356c89272d877877169b313a50044d23570a1
Jayden Kim <hkim@lcs.on.ca>
2025-02-12 18:28:20 -0500

    Initial Commit

---
+++ Plus/PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlusApp.swift
+//  Plus
+//
+//  Created by kim heejun on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
